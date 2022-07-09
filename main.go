551a41d49173fbadbc50a1a3f632fc5d4ae210c7
Danilo Monteiro <dmonteiro.souza@gmail.com>
2022-07-09 07:46:54 -0300

    Remove fmt.Println

--- main.go
+++ main.go
@@ -1,69 +1,58 @@
 package main
 
 import (
-	"sync"
 	"bufio"
-	"net/http"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
-	"io/ioutil"
+	"sync"
 )
 
-func main(){
-	// fmt.Println("\n")
-	fmt.Println("frequester tool By tojojo !!")
-	fmt.Println("\\__(-_-)__/")
-	// fmt.Println("\n")
-
+func main() {
 	colorReset := "\033[0m"
 	colorRed := "\033[31m"
-    colorGreen := "\033[32m"
-
 
 	sc := bufio.NewScanner(os.Stdin)
 
 	jobs := make(chan string)
 	var wg sync.WaitGroup
 
-	for i:= 0; i < 20; i++{
-
+	for i := 0; i < 20; i++ {
 		wg.Add(1)
-		go func(){
-			defer wg.Done()
-			for domain := range jobs {
 
+		go func() {
+			defer wg.Done()
+
+			for domain := range jobs {
 				resp, err := http.Get(domain)
-				if err != nil{
+
+				if err != nil {
 					continue
 				}
+
 				body, err := ioutil.ReadAll(resp.Body)
+
 				if err != nil {
-	      			fmt.Println(err)
-	   			}
-	   			sb := string(body)
-	   			check_result := strings.Contains(sb , "alert(1)")
-	   			// fmt.Println(check_result)
-	   			if check_result != false {
-	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
-	   			}else{
-	   				fmt.Println(string(colorGreen),"Not Vulnerable To XSS:", domain, string(colorReset))
-	   			}
+					fmt.Println(err)
+				}
 
+				sb := string(body)
+				check_result := strings.Contains(sb, "alert(1)")
+
+				if check_result != false {
+					fmt.Println(string(colorRed), "Vulnerable To XSS:", domain, string(colorReset))
+				}
 			}
-			
-   		}()
-
+		}()
 	}
 
-
-
-	for sc.Scan(){
+	for sc.Scan() {
 		domain := sc.Text()
-		jobs <- domain		
-		
-
+		jobs <- domain
 	}
+
 	close(jobs)
 	wg.Wait()
 }
